03482b37f0528115d68497734b59027ed7261e7b
ky92jfks <69474684+ky92jfks@users.noreply.github.com>
2022-08-03 14:28:00 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,6 +13,21 @@ $(function(){
     $('nav a').on('click', function() {
         hamburger();
     });
+
+    /* ページ内スクロール */
+    $('a[href^="#"]').click(function(){
+        //スクロールのスピード
+        var speed = 500;
+        //リンク元を取得
+        var href= $(this).attr("href");
+        //リンク先を取得
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        //リンク先までの距離を取得
+        var position = target.offset().top;
+        //スムーススクロール
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
 });
 
 
